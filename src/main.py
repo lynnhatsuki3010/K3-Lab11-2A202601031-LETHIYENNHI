eb96ecabd29f8f5f81ec72a0d7c91a752633bf50
lynnhatsuki3010 <lyn.nhi3010@gmail.com>
2026-08-07 10:06:54 +0700

    Fix Windows console UTF-8 encoding for Vietnamese output

--- src/main.py
+++ src/main.py
@@ -13,6 +13,10 @@ import sys
 import asyncio
 import argparse
 
+if hasattr(sys.stdout, "reconfigure"):
+    sys.stdout.reconfigure(encoding="utf-8")
+    sys.stderr.reconfigure(encoding="utf-8")
+
 from core.config import setup_api_key
 
 
